dc2f66531c1e09ee991b02678ae7f4b5d8eabfc7
Emmanuel Ameisen <emmanuel.ameisen@gmail.com>
2019-05-01 22:13:39 -0700

    adding calibration curve

--- model_evaluation.py
+++ model_evaluation.py
@@ -1,4 +1,5 @@
-from sklearn.metrics import confusion_matrix, roc_curve, auc
+from sklearn.calibration import calibration_curve
+from sklearn.metrics import confusion_matrix, roc_curve, auc, brier_score_loss
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -49,7 +50,7 @@ def get_roc_plot(predicted_proba_y, true_y):
     https://scikit-learn.org/stable/auto_examples/model_selection/plot_roc_crossval.html
     :param predicted_proba_y: the predicted probabilities of our model for each example
     :param true_y: the true value of the label
-    :return:
+    :return:roc plot
     """
     fpr, tpr, thresholds = roc_curve(true_y, predicted_proba_y)
     roc_auc = auc(fpr, tpr)
@@ -57,4 +58,44 @@ def get_roc_plot(predicted_proba_y, true_y):
              label='ROC curve (AUC = %0.2f)' % roc_auc)
     plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
              label='Chance', alpha=.8)
+    return plt
+
+
+def get_calibration_plot(predicted_proba_y, true_y):
+    """
+    Inspired by sklearn example
+    https://scikit-learn.org/stable/auto_examples/calibration/plot_calibration_curve.html
+    :param predicted_proba_y: the predicted probabilities of our model for each example
+    :param true_y: the true value of the label
+    :return: calibration plot
+    """
+
+    fig = plt.figure()
+    ax1 = plt.subplot2grid((3, 1), (0, 0), rowspan=2)
+    ax2 = plt.subplot2grid((3, 1), (2, 0))
+
+    ax1.plot([0, 1], [0, 1], "k:", label="Perfectly calibrated")
+    clf_score = brier_score_loss(true_y, predicted_proba_y,
+                                 pos_label=true_y.max())
+    print("\tBrier: %1.3f" % clf_score)
+
+    fraction_of_positives, mean_predicted_value = \
+        calibration_curve(true_y, predicted_proba_y, n_bins=10)
+
+    ax1.plot(mean_predicted_value, fraction_of_positives, "s-",
+             label="Brier score: (%1.3f)" % clf_score)
+
+    ax2.hist(predicted_proba_y, range=(0, 1), bins=10,
+             histtype="step", lw=2)
+
+    ax1.set_ylabel("Fraction of positives")
+    ax1.set_ylim([-0.05, 1.05])
+    ax1.legend(loc="lower right")
+    ax1.set_title('Calibration plot  (reliability curve)')
+
+    ax2.set_xlabel("Mean predicted value")
+    ax2.set_ylabel("Count")
+    ax2.legend(loc="upper center", ncol=2)
+
+    plt.tight_layout()
     return plt
