77fecacf57df5dd0fae128b6935133eda1fa3f96
Emmanuel Ameisen <emmanuel.ameisen@gmail.com>
2019-05-19 18:22:45 -0700

    adding ingestion stubs

--- tests/test_ingestion.py
+++ tests/test_ingestion.py
@@ -1,11 +1,23 @@
 import os
 
-from data_transformation import parse_xml_to_csv
+from data_transformation import parse_xml_to_csv, get_random_train_test_split
 from pathlib import Path
 import pandas as pd
 
 
-def test_parser_returns_dataframe():
+def get_fixture_df():
     curr_path = os.path.dirname(__file__)
-    df = parse_xml_to_csv(curr_path / Path("test_data/MiniPosts.xml"))
+    return parse_xml_to_csv(curr_path / Path("test_data/MiniPosts.xml"))
+
+
+def test_parser_returns_dataframe():
+    df = get_fixture_df()
     assert isinstance(df, pd.DataFrame)
+
+
+def test_text_present():
+    raise NotImplementedError
+
+
+def test_have_questions():
+    raise NotImplementedError
