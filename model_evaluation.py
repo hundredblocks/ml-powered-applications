3dc0911d19b67324bcccbd5bf77bb5e54574ec0a
Emmanuel Ameisen <emmanuel.ameisen@gmail.com>
2019-05-27 23:13:23 -0700

    Added get top k

--- model_evaluation.py
+++ model_evaluation.py
@@ -249,3 +249,51 @@ def get_feature_importance(clf, feature_names):
             importances[indices_sorted_by_importance],
         )
     )
+
+
+def get_top_k(df, proba_col, true_label_col, k=5, decision_threshold=0.5):
+    """
+    For binary classification problems
+    Returns k most correct and incorrect example for each class
+    Also returns k most unsure examples
+    :param df: DataFrame containing predictions, and true labels
+    :param proba_col: column name of predicted probabilities
+    :param true_label_col: column name of true labels
+    :param k: number of examples to show for each category
+    :param decision_threshold: classifier decision boundary to classify as positive
+    :return: correct_pos, correct_neg, incorrect_pos, incorrect_neg, unsure
+    """
+    # Get correct and incorrect predictions
+    correct = df[
+        (df[proba_col] > decision_threshold) == df[true_label_col]
+    ].copy()
+    incorrect = df[
+        (df[proba_col] > decision_threshold) != df[true_label_col]
+    ].copy()
+
+    top_correct_positive = correct[correct[true_label_col]].nlargest(
+        k, proba_col
+    )
+    top_correct_negative = correct[~correct[true_label_col]].nsmallest(
+        k, proba_col
+    )
+
+    top_incorrect_positive = incorrect[incorrect[true_label_col]].nsmallest(
+        k, proba_col
+    )
+    top_incorrect_negative = incorrect[~incorrect[true_label_col]].nlargest(
+        k, proba_col
+    )
+
+    # Get closest examples to decision threshold
+    most_uncertain = df.iloc[
+        (df[proba_col] - decision_threshold).abs().argsort()[:k]
+    ]
+
+    return (
+        top_correct_positive,
+        top_correct_negative,
+        top_incorrect_positive,
+        top_incorrect_negative,
+        most_uncertain,
+    )
