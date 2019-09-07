dc5dae93367b21d2235028a45db78bc135dfa652
Emmanuel Ameisen <emmanuel.ameisen@gmail.com>
2019-09-07 14:53:19 -0700

    move data visualization to module

---
+++ ml_editor/data_visualization.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+
+
+def plot_embeddings(embeddings, sent_labels):
+    fig = plt.figure(figsize=(16, 10))
+    color_map = {True: "#1f77b4", False: "#ff7f0e"}
+    plt.scatter(
+        embeddings[:, 0],
+        embeddings[:, 1],
+        c=[color_map[x] for x in sent_labels],
+        s=40,
+        alpha=0.4,
+    )
+
+    handles = [
+        Rectangle((0, 0), 1, 1, color=c, ec="k") for c in ["#1f77b4", "#ff7f0e"]
+    ]
+    labels = ["answered", "unanswered"]
+    plt.legend(handles, labels)
+
+    plt.gca().set_aspect("equal", "box")
+    plt.gca().set_xlabel("x")
+    plt.gca().set_ylabel("y")
+
+    return fig
